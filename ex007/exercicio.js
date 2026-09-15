2eac995d2fd1f3fefabd149973321b10bf9ac8c6
Christian Dessanti <christiandessanti@gmail.com>
2026-09-14 22:53:25 -0300

    insert mais 3 exercicios

---
+++ ex007/exercicio.js
@@ -0,0 +1,6 @@
+var vel = 59.5
+console.log (`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){  //condição simples
+    console.log(`Você passou da velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre com muito cuidado`)
---
+++ ex008/exercicio.js
@@ -0,0 +1,6 @@
+var pais = 'Noruega';
+if(pais == 'Brasil'){
+    console.log("Olá, brasileño")
+} else{
+    console.log("Para gringo é mais caro!")
+}
