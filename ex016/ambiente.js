8a9ad7fb29dc0afdfff976d750702f5a79865f0c
Christian Dessanti <christiandessanti@gmail.com>
2026-09-23 21:37:53 -0300

    insert ex016

---
+++ ex016/ambiente.js
@@ -0,0 +1,11 @@
+var contador = 1
+while(contador <= 1){
+    console.log(`Passo: ${contador} Eduardo Baptista é o novo técnico do Inter`);
+    contador++
+}  // estrutura de repetição com teste lógico no inicio
+
+
+do {
+    console.log('Salve, tropa')
+    contador++
+} while (contador <= 6) // estrutura de repetiçaõ com teste lógico no final
